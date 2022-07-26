468bff3af0024afa283dedcb135d439cf7f39e03
Devraj <195040@nith.ac.in>
2022-07-26 19:41:51 +0530

    deleted extra space

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,14 +1,20 @@
-const { merge } = require('webpack-merge')
+const merge = require('webpack-merge');
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
+
+
 module.exports = merge(
-    commonConfiguration,
+    commonConfiguration, 
     {
-        mode: 'production',
-        plugins:
+    mode: 'production',
+    plugins:
         [
             new CleanWebpackPlugin()
-        ]
+        ],
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
     }
-)
+});
